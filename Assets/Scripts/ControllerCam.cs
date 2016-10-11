58c0a45d8950af07991468d3bd346eddfe8127f7
lord4800 <killerontheroad4800@gmail.com>
2016-10-11 12:25:11 +0300

    first push

---
+++ Assets/Scripts/ControllerCam.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerCam: MonoBehaviour 
+{
+	/*public Rect Rectangle1
+	{
+		get;
+		private set;
+	}*/
+	public GameObject player; //Переменна объекта персонажа с которым будем работать. 
+	//public Texture2D player;
+	//public ch
+	void Start () 
+	{ 
+		
+	} 
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.position = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
+
+	}
+	/*void OnGUI()
+	{
+		GUI.DrawTexture(Rectangle, player);
+	}*/
+}
---
+++ Assets/Scripts/detectedScr.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class detectedScr : MonoBehaviour {
+	Ray r;
+	float f;
+
+	// Use this for initialihation
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate(0,1,0);
+		Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+
+		for (float h = -10.0f; h< 10.0f;h +=0.1f)
+		{
+			RaycastHit[] hitsh;
+			hitsh = Physics.RaycastAll(transform.position, forward + new Vector3(h , 0, 0), 10.0F);//right
+			Debug.DrawRay(transform.position, forward + new Vector3(h, 0, 0), Color.blue);
+			for (int i = 0; i < hitsh.Length; i++)
+			{
+				RaycastHit hit = hitsh[i];
+				Renderer rend = hit.transform.GetComponent<Renderer>();
+				if (rend) 
+				{
+					// Change the material of all hit colliders
+					// to use a transparent shader.
+					rend.material.shader = Shader.Find("Transparent/Diffuse");
+					Color tempColor = rend.material.color;
+					tempColor.a = 0.3F;
+					rend.material.color = tempColor;
+				}
+			}
+		}
+
+		Debug.DrawRay(transform.position, forward, Color.green);
+		RaycastHit hit1;
+		Ray ray = new Ray(transform.position, forward);
+		if (Physics.Raycast(ray, out hit1))
+		{
+			if (hit1.collider != null)
+			{
+				Debug.Log("Find something");
+			}
+		}
+	}
+}
