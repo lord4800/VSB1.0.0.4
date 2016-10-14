de0888802900ce1d3ce3a62856d17e581f111c34
lord4800 <killerontheroad4800@gmail.com>
2016-10-14 23:41:36 +0300

    all project

---
+++ Assets/Scripts/BedTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BedTrigger : MonoBehaviour {
+	bool drink;
+	public Texture tex;
+	void start()
+	{
+		drink = false;
+	}
+
+	void OnTriggerEnter(Collider myTrigger)
+	{
+		if (myTrigger.gameObject.name == "ThirdPersonController")
+		{
+			Debug.LogError ("drink blod");
+			drink = true;
+		}
+		else drink = false;
+	}
+
+	void OnGUI()
+	{
+		if (drink == true) GUI.DrawTexture (new Rect(100,100,100,100), tex);
+
+			
+	}
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour 
+{
+	
+	void OnTriggerEnter(Collider myTrigger)
+	{
+		if (myTrigger.gameObject.name == "ThirdPersonController")
+		{
+			myTrigger.gameObject.transform.position = new Vector3(3f,0.04f,5.8f);
+			Debug.LogError ("hit");
+		}
+	}
+		
+}
