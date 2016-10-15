6fb30a9fc9acdac24a8336f4832ba55ec942d44c
lord4800 <killerontheroad4800@gmail.com>
2016-10-15 11:35:47 +0300

    some fun

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class Trigger : MonoBehaviour 
 {
-	
+	public Vector3 teleportPosition;
 	void OnTriggerEnter(Collider myTrigger)
 	{
 		if (myTrigger.gameObject.name == "ThirdPersonController")
 		{
-			myTrigger.gameObject.transform.position = new Vector3(3f,0.04f,5.8f);
+			//myTrigger.gameObject.transform.position = new Vector3(3f,0.04f,5.8f);
+			myTrigger.gameObject.transform.position = teleportPosition;
 			Debug.LogError ("hit");
 		}
 	}
