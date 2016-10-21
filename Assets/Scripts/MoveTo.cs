b2ac5a6acd7d69bf4bfa759ab6f3b1e773640879
lord4800 <killerontheroad4800@gmail.com>
2016-10-21 20:27:48 +0300

    fucking rigidbody stuff. I make patrol system. enjoy

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -2,21 +2,61 @@
 using System.Collections;
 
 public class MoveTo : MonoBehaviour {
+	private Transform waypoints;
+	private Transform waypoint;
+	private int waypointIndex = -1;
 	public Transform goal;
+	public bool gethere = false;
 	public bool isVisible;
+	public int index;
 	NavMeshAgent agent;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		
+		waypoints =GameObject.Find("WayPoints").transform;
+		NextWaypoint();
 		agent = GetComponent<NavMeshAgent>();
-		moveTo(isVisible);
+		//moveTo(isVisible);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		moveTo(isVisible);
-	}
-	void moveTo(bool Visible)
+	void Update () 
 	{
-		if (Visible) agent.destination = goal.position;
+		//waypoint = waypoints.GetChild(index);
+		//if (agent.remainingDistance == 1) 
+		//NextWaypoint();
+		//moveTo(waypoint);
+		/*if(!gethere) moveTo(waypoint);
+		else 
+		{
+			
+			gethere = false;
+		}*/
+		if (isVisible) moveTo(goal);
+		else
+		{
+			if(!gethere) moveTo(waypoint);
+			else 
+			{
+				NextWaypoint();
+				gethere = false;
+			}
+
+		}
 	}
+	void moveTo(Transform _goal)
+	{
+		agent.destination = _goal.position;
+	}
+	void NextWaypoint()
+	{
+		waypointIndex++;
+		if (waypointIndex == 4)
+		{
+			waypointIndex = 0;			
+		}
+		waypoint = waypoints.GetChild(waypointIndex);
+	}
+
 }
