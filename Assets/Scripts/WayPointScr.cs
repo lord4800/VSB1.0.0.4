9fba2e289a2b5bd96d10a5999358965ca62450e2
lord4800 <killerontheroad4800@gmail.com>
2016-11-11 00:13:49 +0200

    commit one of many (i realy dont know wtf i did from last time)

---
+++ Assets/Scripts/WayPointScr.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WayPointScr : MonoBehaviour {
+	public List<Transform> list;
+	public int localNumberPoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider myTrigger)
+	{
+		if (myTrigger.gameObject.name == "Sphere")
+		{
+			myTrigger.gameObject.GetComponent<MoveTo>().newWayPoint(list);
+		}
+	}
+}
